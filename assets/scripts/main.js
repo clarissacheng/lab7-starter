bcfd1600faff5b9a5595ac05b184fa77ee60ca24
vihsiao <vihsiao@ucsd.edu>
2024-05-15 18:23:58 -0700

    finished expose section and started explore section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,8 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	const recipes = JSON.parse(localStorage.getItem("recipes"));
+	return recipes;
 }
 
 /**
@@ -39,6 +41,14 @@ function addRecipesToDocument(recipes) {
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	const main = document.querySelector("main");
+	console.log(recipes)
+
+	for (let i = 0; i < recipes.length; i++) {
+		const recipeCard = document.createElement("recipe-card");
+		recipeCard.data = recipes[i];
+		main.appendChild(recipeCard);
+	}
 }
 
 /**
@@ -51,6 +61,7 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	localStorage.setItem("recipes", JSON.stringify(recipes));
 }
 
 /**
